6529072081236be44ddc4e98842fed6db5edc048
Lucmaret <baptistelucmaret@wanadoo.fr>
2021-05-10 20:45:31 +0200

    tp_01 exo6

---
+++ tp_01/exo6.php
@@ -0,0 +1,13 @@
+<?php
+
+    $limite = 25;
+
+    for ($j = 1; $j <= $limite; $j += 1) {
+
+        for ($i = 1; $i <= $j; $i += 1) {
+            echo $i . ' ';
+        }
+        echo "<br>"; 
+    } 
+
+?>
