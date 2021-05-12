0720cd3c0cef1462ab672f0adc97c8c4c21d1fd5
Lucmaret <baptistelucmaret@wanadoo.fr>
2021-05-12 23:45:59 +0200

    tp_01 exo8

---
+++ tp_01/exo8.php
@@ -0,0 +1,15 @@
+<?php
+
+    EstPair(22);
+
+    function EstPair($number) {
+
+        if ($number % 2 == 0) {
+            echo "Le nombre entier " . $number . " est pair.";
+        } else {
+            echo "Le nombre entier " . $number . " est impair.";
+        }   
+
+    }
+
+?> 
